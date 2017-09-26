c679af87da8500c9e28dcb86ac5ac0cacd58cfb7
mirzaarifandy <31976919+mirzaarifandy@users.noreply.github.com>
2017-09-26 21:29:34 +0700

    04217028.py
    Tugas Algoritma Dan Pemrograman Kelas

---
+++ tugas1.py
@@ -0,0 +1,9 @@
+nama	= "Ach Mirza Arifandy"
+nim 	= "04217028"
+umur 	= "18"
+
+print("Nama = " +nama)
+print("Nim = " +nim)
+print("Umur = %s Tahun"% (umur))
+print("Umurku 5 Tahun Lagi = %s Tahun"% (int(umur) + 5))
+# print(int(umur)+5)
